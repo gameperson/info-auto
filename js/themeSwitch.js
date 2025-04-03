982c7feda75e06efdf68635898521df7183c8211
gameperson <gamesomeotherday@gmail.com>
2025-04-03 00:11:35 -0700

    Update and rename toggle-theme.js to themeSwitch.js

---
+++ js/themeSwitch.js
@@ -0,0 +1,8 @@
+function initThemeSwitch() {
+    const themeToggle = document.getElementById('theme-toggle');
+    if (themeToggle) {
+        themeToggle.addEventListener('click', () => {
+            document.body.classList.toggle('dark-mode');
+        });
+    }
+}
