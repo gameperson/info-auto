bad78135abaf1663607e894eb4fb66b1fa4600d4
gameperson <gamesomeotherday@gmail.com>
2025-04-03 17:20:25 -0700

    Update themeSwitch.js

--- js/themeSwitch.js
+++ js/themeSwitch.js
@@ -1,22 +1,21 @@
 function initThemeSwitch() {
-    const themeToggle = document.getElementById('theme-toggle');
-    if (themeToggle) {
-        themeToggle.addEventListener('click', () => {
+    const themeToggle = document.querySelectorAll('#theme-toggle');
+    themeToggle.forEach(toggle => {
+        toggle.addEventListener('click', () => {
             document.body.classList.toggle('dark-mode');
-            updateThemeIcon();
+            updateThemeIcon(toggle);
         });
-        updateThemeIcon();
-    }
+        updateThemeIcon(toggle);
+    });
 }
 
-function updateThemeIcon() {
-    const themeToggle = document.getElementById('theme-toggle');
-    if (themeToggle) {
+function updateThemeIcon(toggle) {
+    if (toggle) {
         const isDarkMode = document.body.classList.contains('dark-mode');
         if (isDarkMode) {
-            themeToggle.innerHTML = '<img src="assets/images/bulb.0.svg" alt="Light Mode" style="width: 30px;">';
+            toggle.innerHTML = '<img src="assets/images/bulb.0.svg" alt="Light Mode" style="width: 30px;">';
         } else {
-            themeToggle.innerHTML = '<img src="assets/images/bulb.0.png" alt="Dark Mode" style="width: 30px;">';
+            toggle.innerHTML = '<img src="assets/images/bulb.0.png" alt="Dark Mode" style="width: 30px;">';
         }
     }
 }
