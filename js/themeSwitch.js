bc94e370512e1a887bdd22496077492979b1acdf
gameperson <gamesomeotherday@gmail.com>
2025-04-04 16:18:03 -0700

    Update themeSwitch.js

--- js/themeSwitch.js
+++ js/themeSwitch.js
@@ -1,40 +1,18 @@
 function initThemeSwitch() {
-    const themeToggle = document.querySelectorAll('#theme-toggle');
-    themeToggle.forEach(toggle => {
-        toggle.addEventListener('click', (event) => {
-            event.preventDefault(); // Prevent default link behavior
+    const headerThemeToggle = document.getElementById('header-switch-theme');
+    const footerThemeToggle = document.getElementById('footer-switch-theme');
+
+    if (headerThemeToggle) {
+        headerThemeToggle.addEventListener('click', (event) => {
+            event.preventDefault();
             document.body.classList.toggle('dark-mode');
         });
-    });
-}
+    }
 
-// function initThemeSwitch() {
-//     const themeToggle = document.querySelectorAll('#theme-toggle');
-//     themeToggle.forEach(toggle => {
-//         if (!toggle.dataset.listenerAdded) {
-//             toggle.addEventListener('click', () => {
-//                 document.body.classList.toggle('dark-mode');
-//                 updateThemeIcon(toggle);
-//             });
-//             updateThemeIcon(toggle);
-//             toggle.dataset.listenerAdded = true; // Mark that listener has been added
-//         }
-//     });
-// }
-// 
-// function updateThemeIcon(toggle) {
-//     if (toggle) {
-//         const isDarkMode = document.body.classList.contains('dark-mode');
-//         const img = toggle.querySelector('img');
-// 
-//         if (img) {
-//             if (isDarkMode) {
-//                 img.src = 'assets/images/bulb.0.svg';
-//                 img.alt = 'Light Mode';
-//             } else {
-//                img.src = 'assets/images/bulb.0.png';
-//                 img.alt = 'Dark Mode';
-//             }
-//         }
-//     }
-// }
+    if (footerThemeToggle) {
+        footerThemeToggle.addEventListener('click', (event) => {
+            event.preventDefault();
+            document.body.classList.toggle('dark-mode');
+        });
+    }
+}
