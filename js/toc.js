5eba45025c3701efb72e029b77cb7b23059d2b6f
gameperson <gamesomeotherday@gmail.com>
2025-04-07 16:42:44 -0700

    Update toc.js

--- js/toc.js
+++ js/toc.js
@@ -1,37 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-    checkAndLoadToc();
+    generateToc();
 });
 
-function checkAndLoadToc() {
+function generateToc() {
     const articleContent = document.getElementById("article-content");
     const tocContainer = document.getElementById("table-of-contents");
-    const headers = articleContent.querySelectorAll("h1, h2, h3, h4, h5, h6");
-    if (headers.length > 0) {
-        loadToc(headers);
-        tocContainer.style.display = "block"; // Show TOC if headers exist
-    } else {
-        tocContainer.innerHTML = "";
-        tocContainer.style.display = "none"; // Hide TOC if no headers
-    }
-}
-
-function loadToc(headers) {
     const tocList = document.getElementById("toc-list");
-    const tocTriggerTags = ["Introduction", "Uses and Powers of AI Agents", "Misuse and Pitfalls of AI", "Reading List"];
-    if (tocList) {
+
+    if (!articleContent || !tocContainer || !tocList) return;
+
+    const headers = articleContent.querySelectorAll("h2");
+    if (headers.length > 0) {
         tocList.innerHTML = "";
         headers.forEach(header => {
-            const headerText = header.textContent.trim();
-            if (tocTriggerTags.includes(headerText) || (header.previousSibling && header.previousSibling.nodeType === Node.COMMENT_NODE && header.previousSibling.nodeValue.trim() === "toc-entry")) {
-                const listItem = document.createElement("li");
-                const link = document.createElement("a");
-                link.href = "#" + header.id;
-                link.textContent = header.textContent;
-                listItem.appendChild(link);
-                tocList.appendChild(listItem);
-            }
+            const listItem = document.createElement("li");
+            const link = document.createElement("a");
+            const headerId = header.textContent.trim().toLowerCase().replace(/\s+/g, '-').replace(/[^\w-]+/g, '');
+            header.id = headerId;
+            link.href = "#" + headerId;
+            link.textContent = header.textContent;
+            listItem.appendChild(link);
+            tocList.appendChild(listItem);
         });
+        tocContainer.style.display = "block"; // Show TOC if headers exist
     } else {
-        console.error("TOC list element not found!");
+        tocContainer.style.display = "none"; // Hide TOC if no headers
     }
 }
